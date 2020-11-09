e428a27371cc0e177a1da1dc1604a60add5f5a99
Gundersen <shai.gundersen@gmail.com>
2020-11-09 17:47:33 +0200

    added main client file

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+system_arguments = sys.argv[1:]
+father_ip = system_arguments[0]
+father_port = int(system_arguments[1])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+url = input()
+while(True):
+    s.sendto(url.encode(), (father_ip, father_port))
+    data, addr = s.recvfrom(1024)
+    print(str(data.decode()).strip("\n"), addr)
+    url = input()
+s.close()
